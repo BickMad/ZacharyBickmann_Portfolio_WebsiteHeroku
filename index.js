680db2ba7f472add1f0cb82eff7150b46bdc5bee
Zachary Bickmann <zbickmann@oakland.edu>
2021-12-04 15:47:50 -0500

    Test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const ejs = require("ejs");
 
 const app = express();
-
+//Stuff for server
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true}));
 
