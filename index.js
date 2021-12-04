42eeab11cf23ade3ed8008bd7b20279191fd9516
Zachary Bickmann <zbickmann@oakland.edu>
2021-12-04 15:23:23 -0500

    fixed problem with stuff not commiting

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true}));
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.get("/my_resume", (req, res) => {
+    res.render("/html/my_resume");
+});
+
+app.get("/my_fun_facts", (req, res) => {
+    res.render("/html/my_fun_facts");
+});
+
+app.get("/my_blog", (req, res) => {
+    res.render("/html/my_blog");
+});
+
+app.get("/my_trivia", (req, res) => {
+    res.render("/html/my_trivia");
+});
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('Server is running on ${PORT}'));
