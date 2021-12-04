07f49e37d0eaa0ebca00b8c750542cf0e045855b
Zachary Bickmann <zbickmann@oakland.edu>
2021-12-04 16:21:30 -0500

    still tryign to fix nav bar

--- index.js
+++ index.js
@@ -12,19 +12,19 @@ app.get("/", (req, res) => {
     res.render("index");
 });
 
-app.get("/html/my_resume", (req, res) => {
-    res.render("/html/my_resume");
+app.get("/my_resume", (req, res) => {
+    res.render("/html/my_resume.ejs");
 });
 
-app.get("/html/my_fun_facts", (req, res) => {
+app.get("/my_fun_facts", (req, res) => {
     res.render("/html/my_fun_facts");
 });
 
-app.get("/html/my_blog", (req, res) => {
+app.get("/my_blog", (req, res) => {
     res.render("/html/my_blog");
 });
 
-app.get("/html/my_trivia", (req, res) => {
+app.get("/my_trivia", (req, res) => {
     res.render("/html/my_trivia");
 });
 
