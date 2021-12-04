4264a92b9ed217645a795442e8b28ba22b721c62
Zachary Bickmann <zbickmann@oakland.edu>
2021-12-04 18:34:14 -0500

    seeing what the problem is

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const ejs = require("ejs");
-const bodyParser = require('body-parser')
-
+const bodyParser = require('body-parser');
+const{check, validationResult} = require('express-validator');
 
 const app = express();
 //Stuff for server
