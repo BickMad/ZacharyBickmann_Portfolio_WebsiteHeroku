c4fb69fa9ba478352b1a0f5de617e44f0ef1b8ad
Zachary Bickmann <zbickmann@oakland.edu>
2021-12-04 18:29:18 -0500

    i broke everything

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true}));
 
-const urlencoded = bodyParser.urlencoded({ extended: false})
+
 
 app.use("/public", express.static(__dirname + "/public"));
 
